1ce49715088240de89d961a5deb24226b8ad3969
gary <gary@simulmedia.com>
2016-02-05 12:24:47 -0500

    More chapter 3

---
+++ Chapter_3/motorcycles.py
@@ -0,0 +1,36 @@
+# Gary McClure 2015-02-05
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles = []
+
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+del motorcycles[0]
+print(motorcycles)
+
+

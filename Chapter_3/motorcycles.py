5961687a47b1d7569d176aa5127fc1acbae7bec2
gary <gary@simulmedia.com>
2016-02-07 10:53:22 -0500

    More chapter 3

--- Chapter_3/motorcycles.py
+++ Chapter_3/motorcycles.py
@@ -33,4 +33,46 @@ print(motorcycles)
 del motorcycles[0]
 print(motorcycles)
 
+# Pop Stuff
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+last_owned = motorcycles.pop()
+print("The last motorcycle I owned was a " + last_owned.title() + ".")
+
+
+# Popping items from a list
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+first_owned = motorcycles.pop(0)
+print("The first motorcycle I owned was a " + first_owned.title() + ".")
+
+# Removing an item by value
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+motorcycles.remove('ducati')
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\n " + too_expensive.title() + " is too expensive for me.")
+
+
+
+
 

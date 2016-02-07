b2b8f72d4c68d3676a2badbd41777fa91fefa9ee
gary <gary@simulmedia.com>
2016-02-07 11:55:57 -0500

    More chapter 3

---
+++ Chapter_3/visit.py
@@ -0,0 +1,22 @@
+# Gary McClure 2016-02-07
+
+visit = ['france', 'spain', 'italy', 'arizona','scotland']
+
+print(visit)
+print(sorted(visit))
+print(visit)
+
+print(sorted(visit, reverse=True))
+print(visit)
+
+visit.reverse()
+print(visit)
+
+visit.reverse()
+print(visit)
+
+visit.sort()
+print(visit)
+
+visit.sort(reverse=True)
+print(visit)

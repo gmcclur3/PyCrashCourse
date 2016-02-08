a40ce9f8660c137650f3895c5f22accf6d6ee8db
gary <gary@simulmedia.com>
2016-02-08 10:49:05 -0500

    Start of chapter 4

---
+++ Chapter_4/magicians.py
@@ -0,0 +1,10 @@
+# Gary McClure 2015-02-08
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician + "\nz")
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")

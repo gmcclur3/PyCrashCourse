80b6ce55afd2d74a98a4ea54d94e847012eac842
gary <gary@simulmedia.com>
2016-02-08 15:45:48 -0500

    Finish chapter 4

---
+++ Chapter_4/dimensions.py
@@ -0,0 +1,21 @@
+# Gary McClure 2016-02-08
+
+dimensions = (200, 50)
+print("Original Dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nModified Dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+menu_items = ('beef', 'chicken', 'Pork', 'Buffalo', 'Vegetarian')
+print("\nToday's Menu:")
+for menu in menu_items:
+    print(menu)
+
+menu_items = ('beef', 'chicken', 'Pork', 'Alligator', 'Ostrich')
+print("\nToday's Menu Updated:")
+for menu in menu_items:
+    print(menu)

568c44dac4d659675fb22161683d536594fe622b
gary <gary@simulmedia.com>
2016-02-08 11:11:09 -0500

    More chapter 4

---
+++ Chapter_4/animals.py
@@ -0,0 +1,7 @@
+# Gary McClure 2016-02-08
+
+animals = ['dog', 'cat', 'opossum']
+for animal in animals:
+    print("A " + animal.title() + " would make a great pet")
+
+print("Any of these animals would make a great pet!")
---
+++ Chapter_4/pizzas.py
@@ -0,0 +1,7 @@
+# Gary McClure 2016-02-08
+
+pizzas = ['sausage', 'meatball', 'pepperoni', 'sweet peppers and onions']
+for pizza in pizzas:
+    print("I like " + pizza.title() + " Pizza.")
+
+print("\nI really Like Pizza!")
